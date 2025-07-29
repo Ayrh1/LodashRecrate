3395c7ca15664d208a7dabb17c794cb2090b89ad
ayrh90-fZNzxW <ayrh90@icloud.com>
2025-07-28 19:22:11 -0700

    clamp, inRange, words....done

--- _.js
+++ _.js
@@ -1,14 +1,61 @@
 const _ = {
     clamp(num,lower, upper) {
-        let max = Math.max(num, lower, upper);
-        let min = Math.min(num, lower, upper); 
-    
-        console.log(`Upper:${max} lower: ${min}`);
+        console.log(`before change lower: ${lower} upper: ${upper}`);
+        if(lower > upper){
+            /*
+            let placeholder = lower;
+            lower = upper;
+            upper = placeholder;
+            */
+           //shortcut
+           [lower, upper] = [upper, lower]; 
+            console.log(`lower:${lower} upper:${upper}`);
+        }
+        let result = Math.max(num, lower);
+        let finalValue = Math.min(result, upper);
+        return finalValue
     },
+    inRange(num, start, end) {
+        if (end === undefined) {
+            end = start;
+            start = 0;
+        }
+        if (start > end) {
+            [start, end] = [end, start];
+        }
+        if (num >= start && num < end){
+            return true; 
+        } else {
+            return false;
+        }
+    },
+    words(string) {
+        let wordArray = string.split(" ");
+        return wordArray;
+
+    },
+    pad(string, lenght) {
+
+        /*
+        idea 
+            .padddingstart(start)
+            .paddingend()
+
+        */
+
+    }, 
 }; 
 
 
-_.clamp(10,5,20);
+//let clampValue = _.clamp(1,5,-10);
+//console.log(clampValue); 
+
+//let inRangeValue = _.inRange(3,2); 
+//console.log(inRangeValue); 
+
+//let arrayOfWords = _.words('La cucuaracha ya no puede caminar'); 
+//console.log(arrayOfWords);
+
 
 
 
