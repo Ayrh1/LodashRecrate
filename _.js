e6e8eecc63c4b51383c6f989676abd4be6ed2a2e
ayrh90-fZNzxW <ayrh90@icloud.com>
2025-07-31 18:25:50 -0700

    methods done

--- _.js
+++ _.js
@@ -46,8 +46,86 @@ const _ = {
         return finalPad
 
     }, 
+    has(object, value) {
+        
+        if(object.hasOwnProperty(value) === false) {
+            return false;
+        }
+
+        if(object[value] === null || object[value] === undefined){
+            return false; 
+        }
+
+        return true; 
+    },
+    invert(objct) {
+        let newObject = {}; 
+
+        let keyArray = Object.keys(objct);
+        let valueArray = Object.values(objct);
+
+        for(let i = 0; i < keyArray.length; i++){
+
+            //[valueArray[i] -> brackets help access the value inside the array]
+            newObject[valueArray[i]] = keyArray[i]; 
+            
+        }
+
+        return newObject; 
+    }, 
+    findKey(obj, predicate) {
+        for (let key in obj) {
+            const value = obj[key];
+            if (predicate(value)) {
+            return key;
+            }
+        }
+        return undefined;
+    }, 
+    drop(arry, num) {     
+        if(num === undefined){
+            num = 1;
+        }  
+        return arry.slice(num)
+    },
+    dropWhile(array, predicate) {
+        let dropNumber = array.findIndex((element, index) => {return !predicate(element, index, array)});
+        let droppedArray = this.drop(array, dropNumber);
+        
+        return droppedArray;
+    },
+    chunk(array, num) {
+        if(num === undefined) {
+            num = 1; 
+        }
+        let newArrays = []; 
+
+        for(let i = 0; i < array.length; i += num) {
+            let newArray = array.slice(i, i+ num); 
+            newArrays.push(newArray); 
+        }
+        return newArrays; 
+
+    },
 }; 
 
+
+
+/*
+const objectTest = {
+    a: 'abc',
+    b: 45,
+    c: false,
+};
+
+let newObject = _.invert(objectTest); 
+console.log(newObject); 
+*/
+
+/*
+let testObject = _.has(objectTest, 'a');
+console.log(testObject);
+*/
 
 //let clampValue = _.clamp(1,5,-10);
 //console.log(clampValue); 
@@ -58,8 +136,8 @@ const _ = {
 //let arrayOfWords = _.words('La cucuaracha ya no puede caminar'); 
 //console.log(arrayOfWords);
 
-let paddedString = _.pad('hello', 10);
-console.log(paddedString); 
+//let paddedString = _.pad('hello', 10);
+//console.log(paddedString); 
 
 
 // Do not write or modify code below this line.
